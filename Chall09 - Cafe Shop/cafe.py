05687af4e74bb959fb7658af37370e705984eba2
Tobias Reich <37045496+TobjasR@users.noreply.github.com>
2021-05-13 21:06:43 +0200

    Update cafe.py

--- Chall09 - Cafe Shop/cafe.py
+++ Chall09 - Cafe Shop/cafe.py
@@ -3,13 +3,17 @@
 import hashlib
 
 name = "COLA DECAF"
-key1 = "c01a"
-key2 = "decaf"
+c01a = "c01a"
+decaf = "decaf"
 
 for i in range(10000000, 99999999):
-	s = str(i) + " " + name
-	hash = "" + hashlib.sha256(s.encode("utf-8")).hexdigest()
-	if key1 in hash and key2 in hash:
-		print(s)
-		print(hash)
+	id_string = str(i) + " " + name
+	id_hash = hashlib.sha256(id_string.encode()).hexdigest()
+	if c01a in id_hash and decaf in id_hash:
+		print("id_string: " + id_string)
+		print("id_hash: " + id_hash)
 		break
+
+# eventually hits at 37374099 (37%) and outputs:
+# id_string: 37374099 COLA DECAF
+# id_hash: f5ba3332710abaf7d01decaff966e3e6cc579a5274f8be9cf3442d3c01a373fb
